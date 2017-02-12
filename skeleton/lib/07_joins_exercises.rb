638fbc037f124eb4fbb5cbf886e7953fdd531b45
Candra_Tran <candratran@gmail.com>
2017-02-11 17:00:24 -0800

    Finished 07

--- skeleton/lib/07_joins_exercises.rb
+++ skeleton/lib/07_joins_exercises.rb
@@ -112,6 +112,16 @@ def prolific_actors
   # Obtain a list in alphabetical order of actors who've had at least 15
   # starring roles.
   execute(<<-SQL)
+    SELECT
+      DISTINCT actors.name
+    FROM actors
+    JOIN castings ON castings.actor_id = actors.id
+    WHERE ord = 1
+    GROUP BY
+      actors.name
+    HAVING COUNT(castings.*) >= 15
+    ORDER BY
+      actors.name;
   SQL
 end
 
@@ -119,11 +129,41 @@ def films_by_cast_size
   # List the films released in the year 1978 ordered by the number of actors
   # in the cast (descending), then by title (ascending).
   execute(<<-SQL)
+    SELECT
+      movies.title, count(DISTINCT castings.actor_id) as actor_count
+    FROM
+      movies
+    JOIN
+      castings ON castings.movie_id = movies.id
+    WHERE movies.yr = 1978
+    GROUP BY movies.title
+    ORDER BY count(*) DESC, movies.title ASC;
+
   SQL
 end
 
 def colleagues_of_garfunkel
   # List all the people who have played alongside 'Art Garfunkel'.
   execute(<<-SQL)
+    SELECT
+      DISTINCT actors.name
+      FROM(
+      SELECT
+        castings.movie_id
+      FROM
+        castings
+      JOIN actors ON actors.id = castings.actor_id
+      WHERE
+        actors.name = 'Art Garfunkel'
+    ) as selected_movies
+    JOIN
+      movies ON selected_movies.movie_id = movies.id
+    JOIN
+      castings ON movies.id = castings.movie_id
+    JOIN
+      actors on actors.id = castings.actor_id
+    WHERE
+      actors.name != 'Art Garfunkel';
+
   SQL
 end
